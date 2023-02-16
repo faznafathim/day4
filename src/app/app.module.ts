ddc89d24a20cc42357d493d8e5f1cf0d295704cf
faznafathim <fathimafazna3101@gmail.com>
2023-02-17 01:52:18 +0530

    project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { ArtformComponent } from './pages/artform/artform.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { ContactusComponent } from './pages/contactus/contactus.component';
+import { SinglepageComponent } from './pages/singlepage/singlepage.component';
+import { NavbarComponent } from './units/navbar/navbar.component';
+import { FooterComponent } from './units/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    ArtformComponent,
+    LoginComponent,
+    SignupComponent,
+    ContactusComponent,
+    SinglepageComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
